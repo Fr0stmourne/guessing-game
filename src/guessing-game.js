0d9733c6d07c6e750adaa9b178cb72abeccfbe1d
Frostmourne <cozroman@yandex.ru>
2019-09-27 00:06:14 +0300

    feat: implement guessing-game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this._bottomBoundary = min;
+        this._upperBoundary = max;
     }
 
     guess() {
-
+        return Math.ceil((this._bottomBoundary + this._upperBoundary) / 2);
     }
 
     lower() {
-
+        this._upperBoundary = this.guess();
     }
 
     greater() {
-
+        this._bottomBoundary = this.guess();
     }
 }
 
